1f639736775a7b419433c1ba35f1c199733e7a60
André Forner <andrefornerr@gmail.com>
2020-01-29 00:08:57 -0300

    Created class to get information from json

--- app/src/main/java/com/dedemolu/path/utils/RetriviedData.kt
+++ app/src/main/java/com/dedemolu/path/utils/RetriviedData.kt
@@ -1,24 +1,56 @@
 package com.dedemolu.path.utils
 
+import android.content.Context
 import android.util.Log
 import com.dedemolu.path.data.model.Book
+import com.dedemolu.path.data.model.Chapter
+import com.dedemolu.path.data.model.Point
 import com.google.gson.Gson
-import java.io.InputStream
 import java.lang.Exception
 
-class RetriviedData {
+class RetriviedData(
+    private val context: Context
+) {
 
-    companion object {
-        fun dataBooksFromJson(inputStream: InputStream): List<Book> {
-            val gson = Gson()
-            try {
-                val inputString: String = inputStream.bufferedReader().use { it.readText() }
-                return gson.fromJson(inputString, Array<Book>::class.java).toList()
-            } catch (e: Exception) {
-                Log.d("Exception: ", e.toString())
-            }
-            return listOf()
+    private val gson = Gson()
+
+    private fun getBooksFromJson(): List<Book> {
+        try {
+            return gson.fromJson(readFromJson("books.json"), Array<Book>::class.java).toList()
+        } catch (e: Exception) {
+            Log.d("Exception: ", e.toString())
         }
+        return listOf()
     }
+
+    private fun getPointsFromJson(): List<Point> {
+        try {
+            return gson.fromJson(readFromJson("points.json"), Array<Point>::class.java).toList()
+        } catch (e: Exception) {
+            Log.d("Exception: ", e.toString())
+        }
+        return listOf()
+    }
+
+    private fun getChaptersFromJson(): List<Chapter> {
+        try {
+            return gson.fromJson(readFromJson("chapters.json"), Array<Chapter>::class.java).toList()
+        } catch (e: Exception) {
+            Log.d("Exception: ", e.toString())
+        }
+        return listOf()
+    }
+
+    private fun readFromJson(fileName: String): String {
+        val inputStream = context.assets.open(fileName)
+        return inputStream.bufferedReader().use { it.readText() }
+    }
+
+    fun loadAllObjectsFromJson(): List<Book> {
+        getChaptersFromJson()
+        getPointsFromJson()
+        return getBooksFromJson()
+    }
+
 
 }
